2730f49c4915756cb5c278e7b0cbaa3af3cbaa56
Nate B. <121842165+FluffyCowBird@users.noreply.github.com>
2024-12-07 10:21:46 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
-
-// Disable server-side rendering for the planner component
-const TeacherPlanner = dynamic(() => import('../components/TeacherPlanner.jsx'), {
-  ssr: false
-});
+import TeacherPlanner from '@/components/TeacherPlanner';
 
 export default function Home() {
   return <TeacherPlanner />;
