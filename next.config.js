2e48e75a7839ea7e98abe28f66b14e4a4f65d244
Nate B. <121842165+FluffyCowBird@users.noreply.github.com>
2024-12-07 20:01:21 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/teacher-planner',
   assetPrefix: '/teacher-planner/',
   trailingSlash: true,
   output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true
   },
