fc659f2033fc340d86ca602997b13a308ff82899
Nate B. <121842165+FluffyCowBird@users.noreply.github.com>
2024-12-07 19:27:28 -0500

    Create _document.jsx

---
+++ pages/_document.jsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <script src="https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"></script>
+        <script src="https://www.gstatic.com/firebasejs/9.0.0/firebase-auth-compat.js"></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
